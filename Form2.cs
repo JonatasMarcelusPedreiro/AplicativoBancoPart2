d52520f6b8295ea2283e7b57de95655f3c0e4829
Jônatas Marcelus P <135742926+JonatasMarcelusPedreiro@users.noreply.github.com>
2023-07-14 17:19:03 -0300

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula04AppBanco
+{
+    public partial class Form2 : Form
+    {// Juntar dois forms//
+        // variável global//
+
+        public List<string> extratos = new List<string>();
+
+        // public = variável publica//
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // mostrar o que ta salvo nos extratos/loop//
+            // for comando pra loop//
+
+            for (int i = 0; i < extratos.Count; i++)
+            {
+                ListExtratos.Items.Add(extratos[i]);
+                i++;
+
+            }
+
+
+            
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
